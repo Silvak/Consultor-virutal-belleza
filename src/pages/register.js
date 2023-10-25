bbc1ee42f105e9079214a1037ef1dae0042321de
Joan Romero <romerojoan1999@gmail.com>
2023-10-25 12:26:42 -0400

    add register

---
+++ src/pages/register.js
@@ -0,0 +1,9 @@
+import RegisterForm from '@/components/RegisterForm';
+
+export default function Onboarding() {
+	return (
+		<div className="mt-20 w-2/4 m-auto">
+			<RegisterForm />
+		</div>
+	);
+}
