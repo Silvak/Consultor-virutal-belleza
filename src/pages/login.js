1869277f8163f0f269918db6e6008fbf29d4b409
Joan Romero <romerojoan1999@gmail.com>
2023-10-25 12:34:00 -0400

    fix login and register names

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,7 @@
 import LoginForm from '@/components/LoginForm';
 import React from 'react';
 
-export default function Onboarding() {
+export default function Login() {
 	return (
 		<div className="mt-20 w-2/4 m-auto">
 			<LoginForm />
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,6 +1,6 @@
 import RegisterForm from '@/components/RegisterForm';
 
-export default function Onboarding() {
+export default function Register() {
 	return (
 		<div className="mt-20 w-2/4 m-auto">
 			<RegisterForm />
