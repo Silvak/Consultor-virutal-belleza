7db47baf4d5136e31cdeadc7062940b097c72380
Joan Romero <romerojoan1999@gmail.com>
2023-10-24 21:55:57 -0400

    add documentation to api file

--- src/services/api.js
+++ src/services/api.js
@@ -3,12 +3,14 @@ import { getSession } from 'next-auth/react';
 
 export const BASE_URL = process.env.BASE_URL ?? '/api';
 
+// Api instance for endpoints that not require Authorization
 export const apiInstance = axios.create({
 	baseURL: BASE_URL,
 });
 
 apiInstance.defaults.headers.common['Content-Type'] = 'application/json';
 
+// Api instance for endpoints that require Authorization
 export const apiInstanceWithAuth = axios.create({
 	baseURL: BASE_URL,
 });
@@ -16,6 +18,7 @@ export const apiInstanceWithAuth = axios.create({
 apiInstanceWithAuth.defaults.headers.common['Content-Type'] =
 	'application/json';
 
+// Intercept request for apiInstanceWithAuth and add token to Authorization header
 apiInstanceWithAuth.interceptors.request.use(async (request) => {
 	const session = await getSession();
 	request.headers.Authorization = `Bearer ${session?.user?.token}`;
