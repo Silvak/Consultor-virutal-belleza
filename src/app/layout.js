2a99fa065d502882138fd06d3498c0e8aafdef47
silvak <silvak.jeg@gmail.com>
2023-10-27 11:09:47 -0400

    fix merge incompatibilities

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,15 @@
-"use client";
-
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
-import Navbar from "@/components/navigation/Navbar";
 
 //themes
 import { ThemeProvider } from "@/components/CustomThemeProvider";
 import "./globals.css";
 import Providers from "@/app/providers";
 import { getServerSession } from "next-auth";
-import Navbar from "@/components/navigation/Navbar";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
-const metadata = {
+export const metadata = {
   title: "Consultor belleza virtual",
   description: "",
 };
@@ -30,13 +26,12 @@ export default async function RootLayout({ children }) {
             enableSystem
             disableTransitionOnChange
           >
-            {children}
-            <Navbar />
             <div className="flex justify-center w-full mt-[56px]">
               <article className="max-w-[1200px] w-full mx-auto px-4 border-x overflow-hidden ">
                 {children}
               </article>
             </div>
+            <Toaster />
           </ThemeProvider>
         </Providers>
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import "./globals.css";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home user
+      Landing Page
     </main>
   );
 }
