408f665c278d195e34e7438b317505f3eaf7f215
silvak <silvak.jeg@gmail.com>
2023-10-27 11:17:46 -0400

    fix logout btn & add navbar in dashboard

--- src/app/(adminviews)/layout.js
+++ src/app/(adminviews)/layout.js
@@ -1,8 +1,10 @@
+import Navbar from "@/components/navigation/Navbar";
+
 export default function RootLayout({ children }) {
   return (
     <>
       {/* Nav user */}
-
+      <Navbar />
       {/* Page Content */}
       <div className="flex justify-center w-full  mt-[56px]">
         <article className="max-w-[1200px] mx-auto py-8 border-x border-gray-100 overflow-hidden">
--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { DarkMode } from "../ModeToggleTheme";
 import HorizontalMenu from "./HorizontalMenu";
 import SidebarContent from "./SidebarContent";
-import { signOut, useSession } from "next-auth/react";
 
 //cn components
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
@@ -51,9 +50,6 @@ export const routes = [
  * @returns The function `Navbar` returns a JSX element representing the navigation bar component.
  */
 export default function Navbar() {
-  const { data: session } = useSession();
-
-  console.log(session);
   return (
     <header className="fixed top-0 left-0 w-screen h-[60px] border-b border-gray-300/50 bg-white-40 backdrop-blur-md">
       <nav className="flex w-full h-full justify-between items-center px-8">
@@ -79,11 +75,6 @@ export default function Navbar() {
             </SheetTrigger>
 
             <SheetContent className="w-[400px] sm:w-[540px] p-0">
-              {/* 
-				<button className="bg-red-500 hover:bg-red-600 text-white font-semibold rounded-sm px-4 py-2" onClick={() => signOut()}>
-					Logout
-				</button> 
-  				*/}
               <SidebarContent routes={routes} />
             </SheetContent>
           </Sheet>
