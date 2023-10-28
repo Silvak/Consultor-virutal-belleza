936e929182b85028612f6b0955594e9ef3b0f91f
silvak <silvak.jeg@gmail.com>
2023-10-28 18:24:41 -0400

    add responsive to carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,44 +1,54 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { Button } from "./ui/button";
 
+/* The code is defining a React functional component called `Carousel`. It takes a prop called `data`,
+which is an array of objects representing the items to be displayed in the carousel. */
 export default function Carousel({ data }) {
   const [activeSlide, setActiveSlide] = useState(0);
 
-  const emptyCardsCount =
-    3 - (data.length % 3) === 3 ? 0 : 3 - (data.length % 3);
-  const totalSlides = data.length + emptyCardsCount;
+  useEffect(() => {
+    const slideTimer = setInterval(() => {
+      nextSlide();
+    }, 5000);
+
+    return () => {
+      clearInterval(slideTimer);
+    };
+  }, [activeSlide]);
 
   const nextSlide = () => {
-    const newIndex = activeSlide + 3 >= totalSlides ? 0 : activeSlide + 3;
+    const newIndex = activeSlide + 1 >= data.length - 2 ? 0 : activeSlide + 1;
     setActiveSlide(newIndex);
   };
 
   const prevSlide = () => {
-    const newIndex = activeSlide - 3 < 0 ? 0 : activeSlide - 3;
+    const newIndex = activeSlide - 1 < 0 ? data.length - 3 : activeSlide - 1;
     setActiveSlide(newIndex);
   };
 
-  const completeData = [...data];
-  for (let i = 0; i < emptyCardsCount; i++) {
-    completeData.push(data[i % data.length]);
+  // Calculate item width by screen resolution
+  let itemByScreenRes = window.innerWidth < 768 ? 100 : 33.33;
+  if (data && data.length < 3) {
+    itemByScreenRes = 100;
   }
 
   return (
     <div className="relative w-full h-min overflow-x-hidden">
       <div
-        className="flex transition-transform duration-500 ease-in-out"
-        style={{ transform: `translateX(-${activeSlide * 33.33}%)` }}
+        className={`flex transition-transform duration-500 ease-in-out`}
+        style={{ transform: `translateX(-${activeSlide * itemByScreenRes}%)` }}
       >
-        {completeData.map((item, index) => (
+        {/* List items */}
+        {data.map((item, index) => (
           <div
             className="flex flex-col items-center justify-center flex-shrink-0 w-full md:w-[33.33%] h-72 p-2 md:p-4"
             key={index}
           >
-            <div className="relative bg-white w-full h-full overflow-hidden rounded-md shadow-lg cursor-pointer border border-gray-300 hover:border-[#7E8EFF]">
+            <div className="relative bg-white w-full h-full overflow-hidden rounded-md shadow-lg cursor-pointer border border-gray-300 hover:border-[#7E8EFF] select-none">
               {item.image ? (
                 <Image
                   src={item.image}
@@ -51,16 +61,12 @@ export default function Carousel({ data }) {
                 <div className="h-1/2"></div>
               )}
               <div className="absolute top-0 left-0 p-2 md:p-6 h-full w-full flex flex-col justify-end bg-gradient-to-t from-black/60 hover:bg-black/20 text-white">
-                {item.title ? (
-                  <h2 className="text-sm md:text-xl mt-2 md:mt-4 font-semibold">
-                    {item.title}
-                  </h2>
-                ) : null}
-                {item.description ? (
-                  <p className="mt-1 md:mt-2 text-xs md:text-base">
-                    {item.description}
-                  </p>
-                ) : null}
+                <h2 className="text-sm md:text-xl mt-2 md:mt-4 font-semibold">
+                  {item.title}
+                </h2>
+                <p className="mt-1 md:mt-2 text-xs md:text-base">
+                  {item.description}
+                </p>
               </div>
             </div>
           </div>
@@ -69,13 +75,13 @@ export default function Carousel({ data }) {
 
       {/* Navigation buttons */}
       <Button
-        className="hidden md:flex absolute top-1/2 left-2 transform -translate-y-1/2 bg-[#7E8EFF] text-white h-[40px] w-[40px] rounded-md shadow-md p-0 text-xl"
+        className="md:flex absolute top-1/2 left-2 transform -translate-y-1/2 bg-[#7E8EFF] text-white h-[40px] w-[40px] rounded-md shadow-md p-0 text-xl"
         onClick={prevSlide}
       >
         <IoIosArrowBack />
       </Button>
       <Button
-        className="hidden md:flex absolute top-1/2 right-2 transform -translate-y-1/2 bg-[#7E8EFF] text-white h-[40px] w-[40px] rounded-md shadow-md p-0 text-xl"
+        className="md:flex absolute top-1/2 right-2 transform -translate-y-1/2 bg-[#7E8EFF] text-white h-[40px] w-[40px] rounded-md shadow-md p-0 text-xl"
         onClick={nextSlide}
       >
         <IoIosArrowForward />
