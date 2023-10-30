75047f01501f74394cb35eef0a2be0b06e0e86e1
silvak <silvak.jeg@gmail.com>
2023-10-30 00:38:24 -0400

    Changes paddings & input of product page

--- src/app/(userviews)/products/page.js
+++ src/app/(userviews)/products/page.js
@@ -1,63 +1,67 @@
-import Carousel from '@/components/Carousel';
-import ProductsSection from '@/components/ProductsSection';
+import Carousel from "@/components/Carousel";
+import ProductsSection from "@/components/ProductsSection";
 
 const data = [
-	{
-		title: 'Tarjeta 1',
-		description: 'Descripción de la tarjeta 1',
-		image: '/assets/productTest.webp',
-	},
-	{
-		title: 'Tarjeta 2',
-		description: 'Descripción de la tarjeta 1',
-		image: '/assets/productTest.webp',
-	},
-	{
-		title: 'Tarjeta 3',
-		description: 'Descripción de la tarjeta 1',
-		image: '/assets/productTest.webp',
-	},
-	{
-		title: 'Tarjeta 4',
-		description: 'Descripción de la tarjeta 1',
-		image: '/assets/productTest.webp',
-	},
-	{
-		title: 'Tarjeta 5',
-		description: 'Descripción de la tarjeta 1',
-		image: '/assets/productTest.webp',
-	},
-	{
-		title: 'Tarjeta 6',
-		description: 'Descripción de la tarjeta 1',
-		image: '/assets/productTest.webp',
-	},
-	{
-		title: 'Tarjeta 7',
-		description: 'Descripción de la tarjeta 1',
-		image: '/assets/productTest.webp',
-	},
+  {
+    title: "Tarjeta 1",
+    description: "Descripción de la tarjeta 1",
+    image: "/assets/productTest.webp",
+  },
+  {
+    title: "Tarjeta 2",
+    description: "Descripción de la tarjeta 1",
+    image: "/assets/productTest.webp",
+  },
+  {
+    title: "Tarjeta 3",
+    description: "Descripción de la tarjeta 1",
+    image: "/assets/productTest.webp",
+  },
+  {
+    title: "Tarjeta 4",
+    description: "Descripción de la tarjeta 1",
+    image: "/assets/productTest.webp",
+  },
+  {
+    title: "Tarjeta 5",
+    description: "Descripción de la tarjeta 1",
+    image: "/assets/productTest.webp",
+  },
+  {
+    title: "Tarjeta 6",
+    description: "Descripción de la tarjeta 1",
+    image: "/assets/productTest.webp",
+  },
+  {
+    title: "Tarjeta 7",
+    description: "Descripción de la tarjeta 1",
+    image: "/assets/productTest.webp",
+  },
 ];
 
 export default function Page() {
-	return (
-		<div className=" max-w-[1200px] w-full m-auto pt-8">
-			<h1 className="text-2xl font-bold">Busca tus productos</h1>
-			<ProductsSection />
+  return (
+    <div className=" max-w-[1200px] w-full m-auto py-16">
+      <h1 className="text-2xl font-semibold mt-2">Busca tus productos</h1>
+      <ProductsSection />
 
-			<section className="py-8">
-				<h1 className="text-2xl font-bold">Recomendado para ti</h1>
-				<div className="flex justify-center w-full px-0 lg:px-8">
-					<Carousel data={data || []} />
-				</div>
-			</section>
+      <section className="pt-8">
+        <h2 className="text-2xl font-semibold pl-4 mb-4">
+          Recomendado para ti
+        </h2>
+        <div className="flex justify-center w-full">
+          <Carousel data={data || []} />
+        </div>
+      </section>
 
-			<section className="py-8">
-				<h1 className="text-2xl font-bold">Recomendados por especialistas</h1>
-				<div className="flex justify-center w-full px-0 lg:px-8">
-					<Carousel data={data || []} />
-				</div>
-			</section>
-		</div>
-	);
+      <section className="pt-8">
+        <h2 className="text-2xl font-semibold pl-4 mb-4">
+          Recomendados por especialistas
+        </h2>
+        <div className="flex justify-center w-full">
+          <Carousel data={data || []} />
+        </div>
+      </section>
+    </div>
+  );
 }
--- src/components/ProductsSection.jsx
+++ src/components/ProductsSection.jsx
@@ -1,94 +1,94 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import { Input } from './ui/input';
-import { Label } from './ui/label';
-import { Search } from 'lucide-react';
-import useDebounce from '@/hooks/useDebounce';
-import ProductCard from './card/ProductCard';
-import { useQuery } from '@tanstack/react-query';
-import { getProducts } from '@/services/product.services';
-import ProductsSkeletons from './ProductsSkeletons';
-import Carousel from './Carousel';
+import React, { useState } from "react";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Search } from "lucide-react";
+import useDebounce from "@/hooks/useDebounce";
+import ProductCard from "./card/ProductCard";
+import { useQuery } from "@tanstack/react-query";
+import { getProducts } from "@/services/product.services";
+import ProductsSkeletons from "./ProductsSkeletons";
+import Carousel from "./Carousel";
 
 const data = [
-	{
-		title: 'Tarjeta 1',
-		description: 'Descripción de la tarjeta 1',
-		image: '/assets/productTest.webp',
-	},
-	{
-		title: 'Tarjeta 2',
-		description: 'Descripción de la tarjeta 1',
-		image: '/assets/productTest.webp',
-	},
-	{
-		title: 'Tarjeta 3',
-		description: 'Descripción de la tarjeta 1',
-		image: '/assets/productTest.webp',
-	},
-	{
-		title: 'Tarjeta 4',
-		description: 'Descripción de la tarjeta 1',
-		image: '/assets/productTest.webp',
-	},
-	{
-		title: 'Tarjeta 5',
-		description: 'Descripción de la tarjeta 1',
-		image: '/assets/productTest.webp',
-	},
-	{
-		title: 'Tarjeta 6',
-		description: 'Descripción de la tarjeta 1',
-		image: '/assets/productTest.webp',
-	},
-	{
-		title: 'Tarjeta 7',
-		description: 'Descripción de la tarjeta 1',
-		image: '/assets/productTest.webp',
-	},
+  {
+    title: "Tarjeta 1",
+    description: "Descripción de la tarjeta 1",
+    image: "/assets/productTest.webp",
+  },
+  {
+    title: "Tarjeta 2",
+    description: "Descripción de la tarjeta 1",
+    image: "/assets/productTest.webp",
+  },
+  {
+    title: "Tarjeta 3",
+    description: "Descripción de la tarjeta 1",
+    image: "/assets/productTest.webp",
+  },
+  {
+    title: "Tarjeta 4",
+    description: "Descripción de la tarjeta 1",
+    image: "/assets/productTest.webp",
+  },
+  {
+    title: "Tarjeta 5",
+    description: "Descripción de la tarjeta 1",
+    image: "/assets/productTest.webp",
+  },
+  {
+    title: "Tarjeta 6",
+    description: "Descripción de la tarjeta 1",
+    image: "/assets/productTest.webp",
+  },
+  {
+    title: "Tarjeta 7",
+    description: "Descripción de la tarjeta 1",
+    image: "/assets/productTest.webp",
+  },
 ];
 
 function ProductsSection() {
-	const [search, setSearch] = useState('');
-	const debouncedSearch = useDebounce(search, 500);
-	const limit = 6;
+  const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search, 500);
+  const limit = 6;
 
-	const { data: productsData, status } = useQuery({
-		queryKey: ['products', limit, debouncedSearch],
-		queryFn: () =>
-			getProducts({
-				limit,
-				term: debouncedSearch,
-			}),
-	});
+  const { data: productsData, status } = useQuery({
+    queryKey: ["products", limit, debouncedSearch],
+    queryFn: () =>
+      getProducts({
+        limit,
+        term: debouncedSearch,
+      }),
+  });
 
-	return (
-		<div className="my-8">
-			<div className="flex items-center gap-2 w-full">
-				<Input
-					type="text"
-					id="search"
-					value={search}
-					onChange={(e) => {
-						setSearch(e.target.value);
-					}}
-					className="border-none focus-visible:ring-1 h-fit p-1 bg-gray-200"
-				/>
-				<Label htmlFor="search">
-					<Search className="text-gray-400" />
-				</Label>
-			</div>
+  return (
+    <div className="my-8">
+      <div className="flex items-center gap-2 w-full">
+        <Input
+          type="text"
+          id="search"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          className="border-none focus-visible:ring-1 bg-gray-200 dark:bg-gray-200/20 h-[40px] outline-none pl-[40px]"
+        />
+        <Label htmlFor="search" className="absolute pl-2">
+          <Search className="text-gray-400" />
+        </Label>
+      </div>
 
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-8">
-				{status == 'pending' && <ProductsSkeletons />}
-				{status == 'success' &&
-					productsData?.products?.map((product) => (
-						<ProductCard key={product._id} product={product} />
-					))}
-			</div>
-		</div>
-	);
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-8">
+        {status == "pending" && <ProductsSkeletons />}
+        {status == "success" &&
+          productsData?.products?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default ProductsSection;
