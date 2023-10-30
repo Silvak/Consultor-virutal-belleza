749876b4cfc6a6dcfe7b58c1f088809585f1cc64
bradley <108091339+BradleyGCF@users.noreply.github.com>
2023-10-30 14:05:29 -0400

    Login layout

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,11 +1,42 @@
 import LoginForm from "@/components/LoginForm";
+import Link from "next/link";
 import React from "react";
 
 export default function Page() {
   return (
-    <div className="mt-20 w-2/4 m-auto">
-      Login
-      <LoginForm />
+    <div className="flex mt-5 w-full m-auto">
+      {/* Columna izquierda */}
+      <div className="w-1/2 h-screen bg-gray-700 pr-4 rounded-e-md">
+        <div className="flex-1 bg-white w-full px-20 h-screen flex flex-col justify-start items-center">
+          <img
+            src="/assets/login-image.png" // Corregido aquí
+            alt="Imagen login"
+            className="flex w-[300px] h-[400px]"
+          />
+          <h1 className="text-4xl font-bold mb-2">
+            Descubre tu consultor de belleza perfecto
+          </h1>
+          <p className="text-lg">
+            Explora y conecta con tu consultor de belleza ideal. Nuestra
+            aplicación está diseñada para presentarte expertos en belleza que
+            entienden tus necesidades específicas. Descubre recomendaciones
+            personalizadas para cuidado de la piel, consejos de belleza y
+            tendencias actuales.
+          </p>
+        </div>
+      </div>
+
+      {/* Columna derecha */}
+      <div className="flex-1 mt-40 mx-20">
+        <h1 className="font-bold text-3xl mb-6">Iniciar sesión</h1>
+        <LoginForm />
+        <div className="flex justify-center mt-4">
+          <span>No tienes una cuenta? </span>
+          <Link href="/register" legacyBehavior>
+            <a className="text-blue-500 hover:underline">Crear cuenta</a>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,90 +1,91 @@
-'use client';
+"use client";
 
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import * as z from 'zod';
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from './ui/form';
-import { Input } from './ui/input';
-import { Button } from './ui/button';
-import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 // Validation schema
 const loginSchema = z.object({
-	email: z.string().email(),
-	password: z.string(),
+  email: z.string().email(),
+  password: z.string(),
 });
 
 function LoginForm() {
-	const form = useForm({
-		resolver: zodResolver(loginSchema),
-		defaultValues: {
-			email: '',
-			password: '',
-		},
-	});
-	const router = useRouter();
+  const form = useForm({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const router = useRouter();
 
-	async function onSubmit(credentials) {
-		const responseNextAuth = await signIn('credentials', {
-			...credentials,
-			redirect: false,
-		});
+  async function onSubmit(credentials) {
+    const responseNextAuth = await signIn("credentials", {
+      ...credentials,
+      redirect: false,
+    });
 
-		//handle errors
-		if (responseNextAuth?.error) {
-			//
-		}
+    //handle errors
+    if (responseNextAuth?.error) {
+      //
+    }
 
-		//handle success
-		router.push('/home');
-	}
+    //handle success
+    router.push("/home");
+  }
 
-	return (
-		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-				<FormField
-					control={form.control}
-					name="email"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Email</FormLabel>
-							<FormControl>
-								<Input
-									placeholder="email.@example.com"
-									type="email"
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  placeholder="email.@example.com"
+                  type="email"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-				<FormField
-					control={form.control}
-					name="password"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Password</FormLabel>
-							<FormControl>
-								<Input type="password" {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<Button type="submit">Log In</Button>
-			</form>
-		</Form>
-	);
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="Contraseña" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="bg-[#7E8EFF] w-full" type="submit">
+          Iniciar sesion
+        </Button>
+        
+      </form>
+    </Form>
+  );
 }
 
 export default LoginForm;
