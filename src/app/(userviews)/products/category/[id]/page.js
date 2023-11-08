84fd93cbc2b3d254555998482c260651303f2d1b
bradley <108091339+BradleyGCF@users.noreply.github.com>
2023-11-08 13:02:37 -0400

    categories-finished

--- src/app/(userviews)/products/category/[id]/page.js
+++ src/app/(userviews)/products/category/[id]/page.js
@@ -4,53 +4,41 @@ import Carousel from "../../../../../components/Carousel";
 import ProductsSection from "../../../../../components/ProductsSection";
 import RecommendedProductsSection from "../../../../../components/RecommendedProductsSection";
 import { getCategory } from "../../../../../services/category.services";
+import { useQuery } from "@tanstack/react-query";
+import ProductsSkeletons from "../../../../../components/ProductsSkeletons";
+import ProductCard from "../../../../../components/card/ProductCard";
 
+/**
+ * The CategoryWithIdPage function retrieves category data based on the provided id and manages loading
+ * state.
+ */
 const CategoryWithIdPage = ({ params }) => {
   const id = params.id;
-
+  const { data, status } = useQuery({
+    queryFn: () => getCategory(id),
+    queryKey: ["category", id],
+  });
   const [categoryData, setCategoryData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-
-  console.log(id);
-
-  useEffect(() => {
-    if (id) {
-      getCategory(id)
-        .then((response) => {
-          console.log(response);
-          // Suponiendo que la respuesta tenga una estructura { data: { products: [...] } }
-          setCategoryData(response.data);
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          console.error("Hubo un error al obtener la categoría:", error);
-          setIsLoading(false);
-        });
-    }
-  }, [id]);
-
-  if (isLoading) {
-    return <div>Cargando...</div>; // Mostrar un loader o mensaje de carga
-  }
-
-  if (!categoryData) {
-    return <div>No se encontraron datos de la categoría.</div>;
-  }
 
   return (
     <div className="max-w-[1200px] w-full m-auto py-16">
       <h1 className="text-2xl font-semibold mt-2">
         Productos de la categoría: {categoryData.name}
       </h1>
-      {/* Renderizar los productos utilizando ProductsSection si es necesario */}
-      <ProductsSection products={categoryData.products} />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-8">
+        {status == "pending" && <ProductsSkeletons />}
+        {status == "success" &&
+          data?.data?.products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+      </div>
 
       <section className="pt-8">
         <h2 className="text-2xl font-semibold pl-4 mb-4">
           Recomendado para ti
         </h2>
         <div className="flex justify-center w-full">
-          {/* Pasar solo algunos productos al Carousel si quieres mostrar productos destacados */}
           <Carousel data={categoryData.products.slice(0, 5)} />
         </div>
       </section>
